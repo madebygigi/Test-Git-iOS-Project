f45294ed6d6201dd2fc8537a17ea092edf68edb3
gygreene <greneicia@greneicias-imac.attlocal.net>
2014-09-15 19:31:52 -0400

    added test class

---
+++ MBGTest.h
@@ -0,0 +1,13 @@
+//
+//  MBGTest.h
+//  Git Test
+//
+//  Created by Greneicia Greene on 9/15/14.
+//  Copyright (c) 2014 Made by Gigi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBGTest : NSObject
+
+@end
---
+++ MBGTest.m
@@ -0,0 +1,13 @@
+//
+//  MBGTest.m
+//  Git Test
+//
+//  Created by Greneicia Greene on 9/15/14.
+//  Copyright (c) 2014 Made by Gigi. All rights reserved.
+//
+
+#import "MBGTest.h"
+
+@implementation MBGTest
+
+@end
